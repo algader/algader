00dac981c6e16e0e745dbb4d49411b483416f9cf
muhammad abd algader alsukkari <alsukkari.mohammad@icloud.com>
2021-11-30 20:27:20 +0100

    carousel feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './css/style.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import 'jquery/dist/jquery.min';
-import 'popper.js/dist/popper.min.js';
+
+import $ from 'jquery';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min.js'
+import '@fortawesome/fontawesome-free/js/all.min'
+
+
+$(function (){
+    $('[data-toggle="tooltip"]').tooltip()
+    })
+
+
+
+  
+
+   
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,127 @@
 var path = require("path");
-var HtmlwebpackPlugin = require("html-webpack-plugin");
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-
 module.exports = {
     entry: {
-        app: "./src/index.js"
+      app: './src/index.js'
     },
 
     output: {
-        path: path.join(__dirname, "/dist"),
-        publicPath:'',
-        filename: "main.js"
+      path: path.join(__dirname, "/dist"),
+      publicPath: "",
+      filename: "main.js"
     },
-
     mode: "development",
-    devServer:{
-        contentBase:path.join(__dirname,"/dist"),
-        port:1239,
-        writeToDisk:true,
-        open:true,
+    devServer: {
+      contentBase: path.join(__dirname, "/dist"),
+      port: 1239,
+      writeToDisk: true,
+      open: true,
     },
     module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {
-                            minimize: true
-                        }
-                    }
-                ],
-            }, 
-            {
-                test: /\.(png|svg|jpe?g|gif)$/,
-                use: [
-                  {
-                    loader: "file-loader",
-                    options: {
-                      name: "[name].[ext]",
-                      outputPath: "images",
-                    }
-                  }
-                ],
-              },
-              {test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            ],
-          },
-        
 
-    plugins: [
-        new HtmlwebpackPlugin(
+      rules: [
+
+        {
+
+          test: /\.html$/,
+
+          use: [
+
             {
-                filename: "index.html", // فقط  index  و ليس index.html  هنا يجب كتابة اسم الملف مع الامتداد أي
-                template: "./src/index.html"
+
+              loader: "html-loader",
+
+              options: {
+
+                minimize: true,
+
+              }
+
             }
-        ),
-        new MiniCssExtractPlugin(
-            {filename: "css/style.css"}
-        ),
-        new OptimizeCssAssetsPlugin({}),
-    ]
-};
+
+          ]
+
+        },
+
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [{
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '../',
+              },
+            },
+            'css-loader',
+          ]
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [{
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          }]
+
+        },
+
+        {
+          test: /\.(svg|eot|woff|woff2|ttf)$/,
+          use: [
+            {
+              loader: "file-loader", 
+              options: {
+                name: '[name].[ext]',
+                outputPath: "fonts",
+                esModule: false,
+              }
+            }
+          ]
+        },
+
+
+        {
+          test: /\.(svg|eot|woff|woff2|ttf)$/,
+            use: [
+              {
+                loader: "file-loader", 
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: "fonts",
+                  esModule: false,
+                }
+              }
+            ]
+        },
+
+        {
+          test: require.resolve('jquery'),
+          loader: 'expose-loader',
+          options: {
+            exposes: ['$', 'jQuery'],
+          }
+        },
+
+
+
+
+
+
+      ]
+    },
+    plugins:[
+    
+      new HtmlWebpackPlugin({filename:"index.html",
+      
+      template:"./src/index.html",
+      
+      }),
+      new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+      new OptimizeCssAssetsPlugin({}),
+      ],
+
+     
+}
